bce313926a582a2b36c060752a09aaa8938ddb34
mudgalshrawani-debug <mudgalshrawani@gmail.com>
2026-08-19 13:03:21 +0530

    Create students table and insert initial data
    Corrected the INSERT statement syntax for the students table.

---
+++ practical 1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    student_id INT,
+    name VARCHAR(100),
+     marks INT
+);
+INSERT INTO students (student_id, name, marks)
+VALUES (1, 'mehul', 20, ),
+VALUES (2, 'riya', 21, ),
+VALUES (3, 'mit', 19, );
